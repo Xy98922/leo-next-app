07c844d4075ee7008a052a95e079796fd1448ca0
13212365105 <1101527212@qq.com>
2025-03-06 17:43:01 +0800

    修复报错

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <div
           onMouseEnter={() => setContent("🐖")}
           onMouseLeave={() => setContent("***")}
+          onClick={() => setContent("🐖")}
         >
           <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
             大脸妹是
