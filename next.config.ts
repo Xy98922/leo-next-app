937aa119319fa86a411f224c702c48e306fc607f
13212365105 <1101527212@qq.com>
2025-03-06 17:29:42 +0800

    导入项目

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // next.config.js
+  images: {
+    domains: ["images.unsplash.com"], // 允许的图片域名
+  },
 };
 
 export default nextConfig;
